b16aa2887b7fa06a10b210ae9e0d1f1a099e4263
Junaid Khan <junaid.khan@pmdpk.com>
2024-01-04 16:39:48 +0500

    Latest Code..

--- src/main/java/com/example/demo/DemoIntegrationApplication.java
+++ src/main/java/com/example/demo/DemoIntegrationApplication.java
@@ -8,19 +8,45 @@ public class DemoIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoIntegrationApplication.class, args);
-		
+
 		System.out.println("Checking integration completed successfully....");
 
-		System.out.println(String.format("Although some issue exist in the code.... Need to be resovled.", args));
-		
 		printString("CRM Portal Program");
-	}
-	
-	public static void printString(String str) {
+
+		System.out.println(countNumberOfWord("Checking integration completed successfully...."));
 		
-		if(str != null && str.length()>1) {
+		System.out.println(countNumberOfWord(null));
+	}
+
+	public static void printString(String str) {
+
+		if (str != null && str.length() > 1) {
 			System.out.println(str);
 		}
+	}
+
+	public static String countNumberOfWord(String word) {
+
+		int ascii = 0;
+		int count = 1;
+
+		if (word != null && word.length() > 1) {
+			for (int i = 0; i < word.length(); i++) {
+
+				ascii = word.charAt(i);
+
+				if (ascii == 32) {
+					count++;
+				}
+
+			}
+
+			// lst.forEach(s->System.out.print(s));
+
+			return "Word Count :" + count;
+		}
+
+		return "Invalid String provided";
 	}
 
 }
