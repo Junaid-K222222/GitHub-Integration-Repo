1d6bd71145cffef2824dd78a7a8fff0bcc499e5f
Junaid Khan <junaid.khan@pmdpk.com>
2024-01-05 16:14:12 +0500

    Code modify using solid principle.

--- src/main/java/com/example/demo/DemoIntegrationApplication.java
+++ src/main/java/com/example/demo/DemoIntegrationApplication.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.utils.BaseUtils;
 
 @SpringBootApplication
 public class DemoIntegrationApplication {
 
+
+	public static  BaseUtils  baseUtils;
+	
 	/**
 	 * Main Method For Logics
 	 * 
@@ -15,6 +21,8 @@ public class DemoIntegrationApplication {
 		
 		SpringApplication.run(DemoIntegrationApplication.class, args);
 
+		baseUtils = new BaseUtils();
+		
 		// System.out.println("Checking integration completed successfully....");
 		// printString("CRM Portal Program");
 		// System.out.println(countNumberOfWord("Checking integration completed
@@ -23,9 +31,10 @@ public class DemoIntegrationApplication {
 
 		// convertCamelCase("APPLICATION");
 
-		printString("------------------------------------------------------------");
-		System.out.println(convertCamelCaseString("application is now started..."));
-		printString("------------------------------------------------------------");
+		
+		baseUtils.printString("------------------------------------------------------------");
+		System.out.println(baseUtils.convertCamelCaseString("application is now started..."));
+		baseUtils.printString("------------------------------------------------------------");
 
 	}
 
@@ -35,93 +44,93 @@ public class DemoIntegrationApplication {
 	 * @return
 	 */
 	
-	public static String convertCamelCaseString(String str) {
-		StringBuilder newString = new StringBuilder();
-		if (str != null && str.length() > 1) {
-
-			String array[] = str.split(" ");
-			for (int i = 0; i < array.length; i++) {
-				newString.append(makeWordCamelCase(array[i]));
-				newString.append(" ");
-			}
-
-			return newString.toString();
-		}
-
-		return " ";
-		
-	}
-
-	/**
-	 * 
-	 * @param str
-	 * @return
-	 */
-	
-	public static String makeWordCamelCase(String str) {
-
-		if (str != null && str.length() > 1) {
-			str = str.replaceFirst(str.charAt(0) + "", ((char) (((int)str.charAt(0)) - 32)) + "");
-			return str;
-		}
-		
-		return " ";
-	}
-
-	/**
-	 * 
-	 * @param str
-	 * @return
-	 */
-	public static String convertCamelCase(String str) {
-
-		if (str != null && str.length() > 3) {
-
-			for (int i = 0; i < str.length(); i++) {
-
-				System.out.print((char) ((int)str.charAt(i)) + 32 + "==");
-			}
-		}
-
-		return "Invalid String provided";
-	}
-
-	/**
-	 * Used for Print String Pass argument give result
-	 * 
-	 * @param str
-	 */
-	public static void printString(String str) {
-
-		if (str != null && str.length() > 1) {
-			System.out.println(str);
-		}
-	}
-
-	/**
-	 * Check Number of words in a string
-	 * 
-	 * @param word
-	 * @return
-	 */
-	public static String countNumberOfWord(String word) {
-
-		int ascii = 0;
-		int count = 1;
-
-		if (word != null && word.length() > 1) {
-			for (int i = 0; i < word.length(); i++) {
-
-				ascii = word.charAt(i);
-
-				if (ascii == 32) {
-					count++;
-				}
-			}
-			return "Word Count :" + count;
-		}
-
-		return "Invalid String provided";
-	}
+//	public static String convertCamelCaseString(String str) {
+//		StringBuilder newString = new StringBuilder();
+//		if (str != null && str.length() > 1) {
+//
+//			String array[] = str.split(" ");
+//			for (int i = 0; i < array.length; i++) {
+//				newString.append(makeWordCamelCase(array[i]));
+//				newString.append(" ");
+//			}
+//
+//			return newString.toString();
+//		}
+//
+//		return " ";
+//		
+//	}
+//
+//	/**
+//	 * 
+//	 * @param str
+//	 * @return
+//	 */
+//	
+//	public static String makeWordCamelCase(String str) {
+//
+//		if (str != null && str.length() > 1) {
+//			str = str.replaceFirst(str.charAt(0) + "", ((char) (((int)str.charAt(0)) - 32)) + "");
+//			return str;
+//		}
+//		
+//		return " ";
+//	}
+//
+//	/**
+//	 * 
+//	 * @param str
+//	 * @return
+//	 */
+//	public static String convertCamelCase(String str) {
+//
+//		if (str != null && str.length() > 3) {
+//
+//			for (int i = 0; i < str.length(); i++) {
+//
+//				System.out.print((char) ((int)str.charAt(i)) + 32 + "==");
+//			}
+//		}
+//
+//		return "Invalid String provided";
+//	}
+//
+//	/**
+//	 * Used for Print String Pass argument give result
+//	 * 
+//	 * @param str
+//	 */
+//	public static void printString(String str) {
+//
+//		if (str != null && str.length() > 1) {
+//			System.out.println(str);
+//		}
+//	}
+//
+//	/**
+//	 * Check Number of words in a string
+//	 * 
+//	 * @param word
+//	 * @return
+//	 */
+//	public static String countNumberOfWord(String word) {
+//
+//		int ascii = 0;
+//		int count = 1;
+//
+//		if (word != null && word.length() > 1) {
+//			for (int i = 0; i < word.length(); i++) {
+//
+//				ascii = word.charAt(i);
+//
+//				if (ascii == 32) {
+//					count++;
+//				}
+//			}
+//			return "Word Count :" + count;
+//		}
+//
+//		return "Invalid String provided";
+//	}
 
 }
---
+++ src/main/java/com/example/utils/BaseUtils.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.example.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Junaid Khan
+ *
+ */
+@Component
+public class BaseUtils {
+
+	public String convertCamelCaseString(String str) {
+		StringBuilder newString = new StringBuilder();
+		if (str != null && str.length() > 1) {
+
+			String array[] = str.split(" ");
+			for (int i = 0; i < array.length; i++) {
+				newString.append(makeWordCamelCase(array[i]));
+				newString.append(" ");
+			}
+
+			return newString.toString();
+		}
+
+		return " ";
+		
+	}
+
+	/**
+	 * 
+	 * @param str
+	 * @return
+	 */
+	
+	public  String makeWordCamelCase(String str) {
+
+		if (str != null && str.length() > 1) {
+			str = str.replaceFirst(str.charAt(0) + "", ((char) (((int)str.charAt(0)) - 32)) + "");
+			return str;
+		}
+		
+		return " ";
+	}
+
+	/**
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public  String convertCamelCase(String str) {
+
+		if (str != null && str.length() > 3) {
+
+			for (int i = 0; i < str.length(); i++) {
+
+				System.out.print((char) ((int)str.charAt(i)) + 32 + "==");
+			}
+		}
+
+		return "Invalid String provided";
+	}
+
+	/**
+	 * Used for Print String Pass argument give result
+	 * 
+	 * @param str
+	 */
+	public void printString(String str) {
+
+		if (str != null && str.length() > 1) {
+			System.out.println(str);
+		}
+	}
+
+	/**
+	 * Check Number of words in a string
+	 * 
+	 * @param word
+	 * @return
+	 */
+	public  String countNumberOfWord(String word) {
+
+		int ascii = 0;
+		int count = 1;
+
+		if (word != null && word.length() > 1) {
+			for (int i = 0; i < word.length(); i++) {
+
+				ascii = word.charAt(i);
+
+				if (ascii == 32) {
+					count++;
+				}
+			}
+			return "Word Count :" + count;
+		}
+
+		return "Invalid String provided";
+	}
+
+}
